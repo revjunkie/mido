8b20b60b45585bf98b55f894de88b00030e6e925
revjunkie <rajibrahim@rocketmail.com>
2020-02-20 10:58:27 +0800

    gpio: gt9xx: Remove empty brackets
    Signed-off-by: revjunkie <rajibrahim@rocketmail.com>

--- drivers/gpio/gpiolib.c
+++ drivers/gpio/gpiolib.c
@@ -1006,16 +1006,14 @@ static int _gpiod_direction_output_raw(struct gpio_desc *desc, int value)
 #ifdef CONFIG_TOUCHSCREEN_GT9XX_MIDO
 	if ((special_irq == 1) && (gt9xx_flag == 1)) {
 		printk("[GPIO]set GPIO_65 as irq output\n");
-	} else{
-#endif
+	}
+#else
 		if (test_bit(FLAG_USED_AS_IRQ, &desc->flags)) {
 			gpiod_err(desc,
 					"%s: tried to set a GPIO tied to an IRQ as output\n",
 					__func__);
 			return -EIO;
 		}
-#ifdef CONFIG_TOUCHSCREEN_GT9XX_MIDO
-	}
 #endif
 
 	/* Open drain pin should not be driven to 1 */
